027020bdaa9fed43c519f2490720d65211a50346
David <exinmusic@gmail.com>
2018-12-05 18:21:50 -0800

    Init S3 store
    Connecting to an S3 bucket for content and asset storage.

--- rgs/director/views.py
+++ rgs/director/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Document
+from .forms import UploadFileForm
+
 def index(request):
-	return render(request, "director/index.html")
+	form = UploadFileForm
+	return render(request, "director/index.html", {"form": form})
+
+class DocumentCreateView(CreateView):
+    model = Document
+    fields = ['upload', ]
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        documents = Document.objects.all()
+        context['documents'] = documents
+        return context
--- rgs/rgs/settings.py
+++ rgs/rgs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from . import sensitive
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'director',
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,8 +116,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+##### AWS SETTINGS START ######
+
+AWS_ACCESS_KEY_ID = sensitive.key_id
+AWS_SECRET_ACCESS_KEY = sensitive.secret
+AWS_STORAGE_BUCKET_NAME = 'rgs-storage-001'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+#AWS_LOCATION = 'static'
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'mysite/static'),
+#]
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'rgs.storage_backends.MediaStorage'
+
+##### AWS SETTINGS END #####
